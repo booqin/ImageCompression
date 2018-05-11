9655e1d5acc5389d4234c1772280290e23e2d8ed
vito <developervito@163.com>
2018-05-11 14:46:54 +0800

    完善pngcompress

---
+++ README.md
@@ -0,0 +1,64 @@
+## PNGCompress
+该工具通过TinyPNG提供的接口，实现批量图片压缩，可用于app端的资源图片的二次压缩。
+
+## 依赖
+
+需要python3+环境，mac系统可用。
+
+工具类依赖了tinify包
+
+```
+pip install --upgrade tinify
+```
+
+## 用法
+
+压缩需要以下三个参数
+
+- tinify_key：[tinypng官方](https://tinypng.com/)提供的key，注册后即可获得，每月有500张免费压缩量。
+- source_path：源路径
+- target_path：目标路径
+
+其中tinify_key和source_path为必填，target_path空的情况下回默认输出在脚本的同级目录下。
+
+参数可以通过以下两种方式进行配置
+
+### 1.修改目录下的config.json
+在目录下通过config.json，提供配置参数将作为默认值，其中defualt_key需要__添加自己的key__，default_source_path为源路径，default_target_path为目标路径，可不填。
+
+```json
+{
+  "default_key":"tiny_key",
+  "default_source_path":"your source path",
+  "default_target_path":"your target path"
+}
+```
+
+然后直接运行即可：
+
+```python
+
+python3 pngcompress.py
+
+```
+
+### 2.在运行时配置路径
+执行该脚本时输入源路径和目标路径
+
+```python
+
+python3 pngcompress.py /Users/vito/Desktop/test /Users/vito/Desktop/out_png
+
+```
+
+建议使用方法1。
+
+## TODO
+
+- windows的适配
+- python2的适配
+- 压缩速度和提示优化
+
+
+
+
--- pngcompress.py
+++ pngcompress.py
@@ -1,14 +1,16 @@
-#
-#
-
+# -*- coding:utf-8 -*-
+# Create by BoQin on 2018/5/11
 
 import tinify
 import json
 import os
+import sys
 
 source_path_tag = "default_source_path"
 target_path_tag = "default_target_path"
 tinify_key_tag = "default_key"
+
+DEFAULT_OUT = "out_png"
 
 
 def png_compress(*path):
@@ -24,33 +26,81 @@ def png_compress(*path):
         source = tinify.from_file(s_path)
         source.to_file(t_paths[i])
         i += 1
-        print("一共:"+str(len(s_paths))+"; 完成:"+str(i))
+        print("一共:" + str(len(s_paths)) + "; 完成:" + str(i))
 
 
-def get_dirs(s_dir, t_dir):
+def get_all_paths(s_dir, t_dir):
     """
     传入的s_dir为原路径，t_dir为目标路径
     """
+    # 不存在文件夹，创建文件夹
+    if not os.path.exists(t_dir):
+        os.makedirs(t_dir)
+
+    return get_paths(s_dir, t_dir)
+
+
+def get_paths(root_source_dir, root_target_dir):
+    """
+    递归生成所有目标路径
+    :param root_source_dir:
+    :param root_target_dir:
+    :return:
+    """
     source_paths = []
     target_paths = []
-    for dirpath, dirname, filename in os.walk(s_dir):
-        for _file in filename:
-            suffix = os.path.splitext(_file)[1][1:]
-            if suffix == "png":
-                s_path = os.path.join(dirpath, _file)
-                t_path = os.path.join(t_dir, _file)
+
+    for filename in os.listdir(root_source_dir):
+
+        if os.path.isdir(os.path.join(root_source_dir, filename)):
+            # 路径
+            _source_dir = os.path.join(root_source_dir, filename)
+            _target_dir = os.path.join(root_target_dir, filename)
+            # 创建空文件夹
+            make_empty_dir(_target_dir)
+            # 递归文件夹下的文件
+            (s_paths, t_paths) = get_paths(_source_dir, _target_dir)
+            source_paths.extend(s_paths)
+            target_paths.extend(t_paths)
+
+        else:
+            # 文件的情况判断是否为可压缩图片
+            suffix = os.path.splitext(filename)[1][1:]
+            if suffix == "png" or suffix == "jpg":
+                s_path = os.path.join(root_source_dir, filename)
+                t_path = os.path.join(root_target_dir, filename)
                 source_paths.append(s_path)
                 target_paths.append(t_path)
+
     return source_paths, target_paths
+
+
+def make_empty_dir(_dir):
+    if not os.path.exists(_dir):
+        os.makedirs(_dir)
 
 
 def load_config():
     with open('./config.json', 'r') as config:
         return json.load(config)
 
-if __name__ == '__main__':
-    result = load_config()
-    print(result['default_key'])
-    tinify.key = result[tinify_key_tag]
-    png_compress(*get_dirs(result[source_path_tag], result[target_path_tag]))
 
+def get_parm():
+    """
+    获取参数
+    :return:
+    """
+    _result = load_config()
+    source_path = sys.argv[1] if len(sys.argv) > 1 else _result[source_path_tag]
+    target_path = sys.argv[2] if len(sys.argv) > 2 else _result[target_path_tag]
+    if target_path:
+        target_path = os.path.join(os.getcwd(), DEFAULT_OUT)
+    tinify_key = _result[tinify_key_tag]
+
+    return tinify_key, source_path, target_path
+
+
+if __name__ == '__main__':
+    result = get_parm()
+    tinify.key = result[0]
+    png_compress(*get_all_paths(result[1], result[2]))
